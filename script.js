56472b9f8ab9aef524a8528f46a1dd123d67da8d
zhyck1316 <zhyck1316@gmail.com>
2023-03-19 11:56:49 +0800

    Script Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Valerie Faye Arong", "or just call me", "Faye."],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
